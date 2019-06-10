6d6d964ba1d02828f8eeffc0ea34cb97f017e002
Zhuo Chen <zchenpds@users.noreply.github.com>
2019-06-10 13:40:49 -0400

    Improve foot step clustering algorithm

---
+++ matlab/20190415/get_footstep_cluster_indices.m
@@ -0,0 +1,51 @@
+function [i_start, i_end] = get_footstep_cluster_indices(x, y, t)
+
+    % The distance threshold the points within which are considered
+    % inliners of a footstep (foot-flat phase).
+    dist_threshold = 0.05; 
+    % The maximum number of outliers allowed to appear in one footstep
+    max_num_outliers = 3;
+    % The minimum time difference between the startings of two foot-flat
+    % phases
+    min_peak_sep = 0.8;
+    % The maximum duration that one foot-flat phase can last
+    t_threashold = 1;
+    % The minimum number of points required for one foot-flat phase
+    min_num_points = 10;
+    
+    len = length(x);
+    i_start_cand = (1:len)';
+    i_end_cand = i_start_cand + 5;
+
+    for k = 1:len
+        dist_sum = 0;
+        num_outliers = 0;
+        while i_end_cand(k) < len && i_end_cand(k) - i_start_cand(k) < 30 && ...
+                num_outliers < max_num_outliers && ...
+                t(i_end_cand(k)) - t(i_start_cand(k)) < t_threashold
+            dist = norm( [x(i_start_cand(k)) - x(i_end_cand(k)), ...
+                y(i_start_cand(k)) - y(i_end_cand(k))] );
+            dist_sum = dist_sum + dist;
+            i_end_cand(k) = i_end_cand(k) + 1;
+            if dist > dist_threshold
+                num_outliers = num_outliers + 1;
+            end
+        end
+        i_end_cand(k) = i_end_cand(k) - 1;
+    end
+    sep_cand = i_end_cand - i_start_cand;
+    % Debug: show the peaks found
+    figure; findpeaks(sep_cand, t, 'MinPeakHeight', min_num_points, 'MinPeakDistance', min_peak_sep);
+    
+    [~, locs_t] = findpeaks(sep_cand, t, 'MinPeakHeight', min_num_points, 'MinPeakDistance', min_peak_sep);
+    [~, locs] = ismember(locs_t, t);
+    i_start = i_start_cand(locs);
+    i_end = i_end_cand(locs);
+    
+    % Get rid of the footsteps when the subject was actually standing still
+    while length(i_start) >= 2 && ....
+            norm([x(i_start(1)) - x(i_start(2)), y(i_start(1)) - y(i_start(2))]) < dist_threshold
+        i_start = i_start(2:end);
+        i_end = i_end(2:end);
+    end
+end
--- matlab/20190415/test3_2_kinect_only.m
+++ matlab/20190415/test3_2_kinect_only.m
@@ -1,7 +1,11 @@
 %% Initialize variables.
 clear;
 clc;
-[filename1, pathname1] = uigetfile('data*Kinect?.csv');
+close all;
+
+IGNORE_INSOLES = true;
+
+[filename1, pathname1] = uigetfile('data20190517p3dx\data*Kinect?.csv');
 if isequal(filename1,0)
    disp('Cannot open data file. User selected Cancel');
    return
@@ -15,21 +19,29 @@ catch
     dataKinect = importKinectData(fileandpath1, 2, size(dataArray{1}, 1) -1);
 end
 
-[filename2, pathname2] = uigetfile([pathname1,filename1(1:22),'*Insole.mat']);
-if isequal(filename2,0)
-   disp('Neglecting insole data.');
-else
-    load([pathname2,filename2]);
+if IGNORE_INSOLES == false
+    [filename2, pathname2] = uigetfile([pathname1,filename1(1:22),'*Insole.mat']);
+    if isequal(filename2,0)
+        disp('Neglecting insole data.');
+    else
+        disp('Loading insole data.');
+        load([pathname2,filename2]);
+    end
 end
-
-clear pathname1 filename1 pathname2 filename2 
+filenameStance = [pathname1,filename1(1:22),'Stance.mat'];
+clear pathname1 filename1 fileandpath1 pathname2 filename2 
 
 %%
 t0 = dataKinect.tK(1);
 ts = (dataKinect.tK - t0) / 1000;
 
+LineSpecL = {'.-r', 'MarkerSize',5};
+LineSpecLFF = {'s-r', 'MarkerSize',6};
+LineSpecR = {'.-b', 'MarkerSize',5};
+
+
 %% Find heel strike and toe off indices
-if exist('StanceL', 'var')
+if ~IGNORE_INSOLES && exist('StanceL', 'var')
     Stances = {StanceL, StanceR};
     iStances = {nan(size(StanceL, 1), 2), nan(size(StanceR, 1), 2)};
     for n = 1:length(Stances) % loop over {L, R}
@@ -45,7 +57,16 @@ if exist('StanceL', 'var')
         iStances{n}(iNan, :) = [];
     end
     clear Stances Stance
+else
+    [i_start_L, i_end_L] = get_footstep_cluster_indices(dataKinect.WLAnkleX, dataKinect.WLAnkleY, ts);
+    [i_start_R, i_end_R] = get_footstep_cluster_indices(dataKinect.WRAnkleX, dataKinect.WRAnkleY, ts);
+    
+    StanceL = [i_start_L, i_end_L];
+    StanceR = [i_start_R, i_end_R];
+    iStances = {StanceL, StanceR};
+    save(filenameStance, 'StanceL', 'StanceR');
 end
+
 %% Ankles
 xL = dataKinect.WLAnkleX;
 yL = dataKinect.WLAnkleY;
@@ -53,18 +74,16 @@ zL = dataKinect.WLAnkleZ;
 xR = dataKinect.WRAnkleX;
 yR = dataKinect.WRAnkleY;
 zR = dataKinect.WRAnkleZ;
-close all;
 figure;
 ax1 = subplot(3, 1, 1);
-h11 = plot(ts, xL, '.-r');
-hold on;
-h12 = plot(ts, xR, '.-b');
-if exist('StanceL', 'var')
-    plot(ts(iStances{1}(:,1)), xL(iStances{1}(:,1)), 'ro');
-    plot(ts(iStances{1}(:,2)), xL(iStances{1}(:,2)), 'rx');
-    plot(ts(iStances{2}(:,1)), xR(iStances{2}(:,1)), 'bo');
-    plot(ts(iStances{2}(:,2)), xR(iStances{2}(:,2)), 'bx');
-end
+h11 = plot(ts, xL, LineSpecL{:}); hold on;
+h12 = plot(ts, xR, LineSpecR{:});
+
+plot(ts(iStances{1}(:,1)), xL(iStances{1}(:,1)), 'ro');
+plot(ts(iStances{1}(:,2)), xL(iStances{1}(:,2)), 'rx');
+plot(ts(iStances{2}(:,1)), xR(iStances{2}(:,1)), 'bo');
+plot(ts(iStances{2}(:,2)), xR(iStances{2}(:,2)), 'bx');
+    
 grid on;
 legend([h11, h12], "Left", "Right")
 ylabel("X Coordinates (m)")
@@ -72,15 +91,14 @@ xlabel("Time (s)");
 title(sprintf("Ankle, %.2f %%", length(ts)/30/(ts(end) - ts(1)) * 100));
 
 ax2 = subplot(3, 1, 2);
-h11 = plot(ts, yL, '.-r');
-hold on;
+h11 = plot(ts, yL, '.-r');hold on;
 h12 = plot(ts, yR, '.-b');
-if exist('StanceL', 'var')
-    plot(ts(iStances{1}(:,1)), yL(iStances{1}(:,1)), 'ro');
-    plot(ts(iStances{1}(:,2)), yL(iStances{1}(:,2)), 'rx');
-    plot(ts(iStances{2}(:,1)), yR(iStances{2}(:,1)), 'bo');
-    plot(ts(iStances{2}(:,2)), yR(iStances{2}(:,2)), 'bx');
-end
+
+plot(ts(iStances{1}(:,1)), yL(iStances{1}(:,1)), 'ro');
+plot(ts(iStances{1}(:,2)), yL(iStances{1}(:,2)), 'rx');
+plot(ts(iStances{2}(:,1)), yR(iStances{2}(:,1)), 'bo');
+plot(ts(iStances{2}(:,2)), yR(iStances{2}(:,2)), 'bx');
+
 grid on;
 legend([h11, h12], "Left", "Right")
 ylabel("Y Coordinates (m)")
